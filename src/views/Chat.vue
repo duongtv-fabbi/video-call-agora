827de8529aee41054382e040e0446d4bde7f157b
duongtv <duongtv2712@gmail.com>
2022-09-05 11:16:42 +0700

    a

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -94,7 +94,7 @@
                     <a
                       href="javascript:void(0);"
                       class="btn btn-outline-secondary"
-                    ><i class="fa fa-camera"></i
+                    ><i class="fa fa-camera" @click="videoCall()"></i
                     ></a>
                     <a
                       href="javascript:void(0);"
@@ -111,6 +111,9 @@
                     ></a>
                   </div>
                 </div>
+              </div>
+              <div class="video-calling" id="video-calling">
+                <div class="your-video-calling" id="your-video-calling"></div>
               </div>
               <div class="chat-history" id="chat-history">
                 <ul class="m-b-0" v-if="messages.length > 0" id="list-message">
@@ -167,7 +170,7 @@ import moment from "moment";
 import { mapGetters } from "vuex";
 import Vue from "vue";
 import router from "@/router";
-
+import AgoraRTC from "agora-rtc-sdk-ng";
 
 export default {
   name: "Chat",
@@ -184,10 +187,68 @@ export default {
         name: "",
         roomId: "",
         createdAt: ""
+      },
+      rtc: {
+        localAudioTrack: null,
+        localVideoTrack: null,
+        client: null
+      },
+      options: {
+        appId: "0ece007cba7e4af4a1b7b8bb98f1c5a0",
+        // Set the channel name.
+        channel: "duong123",
+        // Pass your temp token here.
+        token: "007eJxTYJCZJ1wm3ZKX6bMyaFXqSTGb2ZwcBsItpjmOHh1TG2vWTFZgMEhNTjUwME9OSjRPNUlMM0k0TDJPskhKsrRIM0w2TTQITRRNvrpJLPluHgsTIwMEgvgcDCml+XnphkbGDAwA3JkfPg==",
+        // Set the user ID.
+        uid: new Date().getTime().toString()
       }
     };
   },
   async created() {
+    // Create an AgoraRTCClient object.
+    this.rtc.client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+
+    // Listen for the "user-published" event, from which you can get an AgoraRTCRemoteUser object.
+    this.rtc.client.on("user-published", async (user, mediaType) => {
+      // Subscribe to the remote user when the SDK triggers the "user-published" event
+      await this.rtc.client.subscribe(user, mediaType);
+      console.log("subscribe success");
+
+      // If the remote user publishes a video track.
+      if (mediaType === "video") {
+        // Get the RemoteVideoTrack object in the AgoraRTCRemoteUser object.
+        const remoteVideoTrack = user.videoTrack;
+        // Dynamically create a container in the form of a DIV element for playing the remote video track.
+        const remotePlayerContainer = document.createElement("div");
+        // Specify the ID of the DIV container. You can use the uid of the remote user.
+        remotePlayerContainer.id = user.uid.toString();
+        remotePlayerContainer.textContent = "Remote user " + user.uid.toString();
+        remotePlayerContainer.style.width = "100%";
+        remotePlayerContainer.style.height = "100%";
+        document.getElementById("video-calling").append(remotePlayerContainer);
+
+        // Play the remote video track.
+        // Pass the DIV container and the SDK dynamically creates a player in the container for playing the remote video track.
+        remoteVideoTrack.play(remotePlayerContainer);
+      }
+
+      // If the remote user publishes an audio track.
+      if (mediaType === "audio") {
+        // Get the RemoteAudioTrack object in the AgoraRTCRemoteUser object.
+        const remoteAudioTrack = user.audioTrack;
+        // Play the remote audio track. No need to pass any DOM element.
+        remoteAudioTrack.play();
+      }
+
+      // Listen for the "user-unpublished" event
+      this.rtc.client.on("user-unpublished", user => {
+        // Get the dynamically created DIV container.
+        const remotePlayerContainer = document.getElementById(user.uid);
+        // Destroy the container.
+        remotePlayerContainer.remove();
+      });
+    });
+
     //from your component
     this.roomId = this.$route.query.roomId ?? null;
     const orderByRooms = query(collection(firebase.db, "rooms"),
@@ -262,6 +323,29 @@ export default {
       }
     },
 
+    async videoCall() {
+      // Join an RTC channel.
+      await this.rtc.client.join(this.options.appId, this.options.channel, this.options.token, this.options.uid);
+      // Create a local audio track from the audio sampled by a microphone.
+      this.rtc.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+      // Create a local video track from the video captured by a camera.
+      this.rtc.localVideoTrack = await AgoraRTC.createCameraVideoTrack();
+      // Publish the local audio and video tracks to the RTC channel.
+      await this.rtc.client.publish([this.rtc.localAudioTrack, this.rtc.localVideoTrack]);
+      // Dynamically create a container in the form of a DIV element for playing the local video track.
+      const localPlayerContainer = document.createElement("div");
+      // Specify the ID of the DIV container. You can use the uid of the local user.
+      localPlayerContainer.id = this.options.uid;
+      localPlayerContainer.textContent = "Local user " + this.options.uid;
+      localPlayerContainer.style.width = "100%";
+      localPlayerContainer.style.height = "100%";
+      document.getElementById("your-video-calling").append(localPlayerContainer);
+      // Play the local video track.
+      // Pass the DIV container and the SDK dynamically creates a player in the container for playing the local video track.
+      this.rtc.localVideoTrack.play(localPlayerContainer);
+      console.log("publish success!");
+    },
+
     async onSubmit() {
       this.msg.createdAt = new Date();
       this.msg.sender = this.user.uid;
@@ -310,7 +394,6 @@ export default {
     },
     scrollToElement() {
       if (document.getElementById("list-message")) {
-        console.log(document.getElementById("list-message").scrollHeight);
         document.getElementById("chat-history").scrollTop = document.getElementById("list-message").scrollHeight;
       }
     },
@@ -370,6 +453,20 @@ export default {
 </script>
 
 <style>
+.your-video-calling{
+  width: 30%;
+  right: 0;
+  height: 19vh;
+  background: red;
+  position: absolute;
+  z-index: 1;
+}
+.video-calling {
+  width: 100%;
+  height: 80vh;
+  position: relative;
+}
+
 textarea {
   resize: none;
 }
@@ -512,6 +609,7 @@ body {
 }
 
 .chat .chat-history {
+  display: none;
   padding: 20px 20px 110px 20px;
   border-bottom: 2px solid #fff;
   max-height: calc(100vh - 180px);
@@ -603,7 +701,8 @@ input[type=text], input[type=password] {
   padding: 18px 0;
   width: 100%;
 }
-.chat .chat-history .image-mess img{
+
+.chat .chat-history .image-mess img {
   min-height: 95px;
 }
 
